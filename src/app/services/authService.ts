efe74c27dfa6b9c2b9996b26fba336bf0778b730
Antonio Saborido <antonio.saborido01@gmail.com>
2024-07-06 16:40:24 +0200

    first commit

---
+++ src/app/services/authService.ts
@@ -0,0 +1,107 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {catchError, Observable, of, tap, throwError} from 'rxjs';
+import {API_URL} from "../../config";
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private sessionUrl = `${API_URL}/api/auth/session`;
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  checkSession(): Observable<boolean> {
+    return this.http.get<boolean>(this.sessionUrl);
+  }
+  public isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+  getToken(): string | null {
+    return localStorage.getItem('accessToken');
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+  removeToken(): void {
+    localStorage.removeItem('token');
+  }
+  private loginUrl = `${API_URL}/api/auth/login`;
+  login(credentials: {username: string, password: string}): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      responseType: 'json' as 'json'
+    };
+
+    return this.http.post<any>(this.loginUrl, credentials, httpOptions).pipe(
+      tap((response: any) => {
+        localStorage.setItem('accessToken', response.accessToken);
+        localStorage.setItem('tokenType', response.tokenType);
+        localStorage.setItem('username', credentials.username);
+        console.log('Inicio de sesión exitoso');
+      }),
+      catchError(error => {
+        console.error('Error durante el inicio de sesión', error);
+        return throwError(error);
+      })
+    );
+  }
+  private registerUrl = `${API_URL}/api/auth/register`;
+
+  register(user: {username: string, password: string}): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      responseType: 'text' as 'json'
+    };
+
+    return this.http.post<string>(this.registerUrl, user, httpOptions).pipe(
+      tap(() => {
+        console.log('Registro exitoso');
+      }),
+      catchError(error => {
+        console.error('Error durante el registro', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  getUsername(): string | null {
+    return localStorage.getItem('username');
+    console.log('Nombre de usuario:', localStorage.getItem('username'));
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('tokenType');
+    localStorage.removeItem('username');
+
+  }
+
+  checkTokenValidity(): Observable<boolean> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.getToken()
+      })
+    };
+
+    return this.http.get<boolean>(this.sessionUrl, httpOptions).pipe(
+      catchError(error => {
+        this.logout();
+        return of(false);
+      })
+    );
+  }
+  private adminUrl = `${API_URL}/api/auth/is_admin`;
+  isAdmin(): Observable<boolean> {
+    const headers = { 'Authorization': 'Bearer ' + this.getToken() };
+    return this.http.get<boolean>(this.adminUrl, { headers }).pipe(
+      catchError(error => {
+        console.error('Error checking admin status', error);
+        return of(false);
+      })
+    );
+  }
+}
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = 'localhost';
+export const API_PORT = '8080';
+export const PROTOCOLO = 'http';
+export const API_URL = `${PROTOCOLO}://${BASE_URL}:${API_PORT}`;
+export const wsURL = `ws://${BASE_URL}:${API_PORT}/wss`;
